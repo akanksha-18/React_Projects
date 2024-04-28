8bdb7dc53952879343ae0e8e360638b9cc37dbc8
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-04-28 19:54:01 +0530

    food

---
+++ FoodWebPage/Food/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/hero/Hero'
+import Second from './Components/second/Second'
+import Cards from './Components/cards/Cards'
+import Footer from './Components/footer/Footer'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+    <Second/>
+    <Cards/>
+    <Footer/>
+    </>
+   
+  )
+}
+
+export default App
---
+++ FoodWebPage/Food/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import styles from '../Navbar/Navbar.module.css';
+import logo from '../Assets/logo.png';
+function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className={styles.navbar}>
+      {/* <a href="#" className={styles.logo}>GeekFoods</a>   */}
+      <img src={logo} className={styles.logo} alt="" />
+      <nav className={`${styles.navCenter} ${showMenu ? styles.showMenu : ''}`}>
+        <ul className={styles.navLinks}>
+          <li><a href="#" className='active'>Home</a></li>
+          <li><a href="#">Quote</a></li>
+         
+          <li><a href="#">Restaurant</a></li>
+          <li><a href="#">Foods</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <button className={styles.getStarted}>Get Started</button>
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        <div className={`${styles.menuLine} ${showMenu ? styles.open : ''}`}></div>
+        <div className={`${styles.menuLine} ${showMenu ? styles.open : ''}`}></div>
+        <div className={`${styles.menuLine} ${showMenu ? styles.open : ''}`}></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
